cd437ed5233aab9f9fec285638760738094c3a02
BOOK4\csy30 <csy1417@naver.com>
2025-05-07 16:54:07 +0900

    2025/05/07 섹션8 조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,16 +4,19 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     // final 키워드의 장점 => 선언시 초기화 혹은 생성자로만 내용이 채워짐
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
 
+    // @RequiredArgsConstructor는 final 키워드가 붙은 필드를 모아서 자동적으로 생성자를 생성
     // 생성자가 한개만 존재할 시 자동적으로 @Autowired가 반영된다
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
